2334c9bfe6fc40d942f1a9e426344bcb8dc6163f
matt123miller <matt123miller@hotmail.co.uk>
2016-08-08 18:36:12 +0100

    Discovered why it wasn't working 2 commits ago, I was using || instead of && so I was having array OoB erros due to excessive method calls.

--- Classes/MeshGenerator.cs
+++ Classes/MeshGenerator.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public static class MeshGenerator
 {
-
     public static MeshData GenerateTerrainMesh(float[,] heightMap)
     {
         int width = heightMap.GetLength(0);
@@ -22,49 +21,56 @@ public static class MeshGenerator
                 meshData.vertices[vertIndex] = new Vector3(topLeftX + x, heightMap[x, y], topLeftZ - y);
                 meshData.uvs[vertIndex] = new Vector2(x / (float)width, y / (float)height);
 
-                if (x < width - 1 && y < height - 1)
+                if ((x < width - 1) && (y < height - 1))
                 {
                     meshData.AddTriangle(vertIndex, vertIndex + width + 1, vertIndex + width);
                     meshData.AddTriangle(vertIndex + width + 1, vertIndex, vertIndex + 1);
                 }
+
                 vertIndex++;
             }
         }
+
         return meshData;
     }
 }
 
+
 public class MeshData
 {
+
     public Vector3[] vertices;
     public int[] triangles;
     public Vector2[] uvs;
 
-    int triangleIndex = 0;
+    public int vertIndex = 0;
 
     public MeshData(int meshWidth, int meshHeight)
     {
+
         vertices = new Vector3[meshWidth * meshHeight];
-        uvs = new Vector2[meshWidth * meshHeight];
         triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+        uvs = new Vector2[meshWidth * meshHeight];
     }
 
     public void AddTriangle(int a, int b, int c)
     {
-        triangles[triangleIndex] = a;
-        triangles[triangleIndex + 1] = b;
-        triangles[triangleIndex + 2] = c;
-        triangleIndex += 3;
+
+        triangles[vertIndex] = a;
+        triangles[vertIndex + 1] = b;
+        triangles[vertIndex + 2] = c;
+        vertIndex += 3;
     }
 
     public Mesh CreateMesh()
     {
+
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.uv = uvs;
         mesh.RecalculateNormals();
+
         return mesh;
     }
-
 }

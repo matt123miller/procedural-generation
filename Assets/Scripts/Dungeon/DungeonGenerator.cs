b854df39d41d8cfb600a9fa5dbe6cefd012df564
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-06-16 23:45:51 +0100

    Working on some generation ideas and patterns. No idea what I'm doing yet.

---
+++ Assets/Scripts/Dungeon/DungeonGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Dungeon
+{
+    class DungeonGenerator : MonoBehaviour
+    {
+        public int totalRooms = 10;
+
+        private void Awake()
+        {
+            GenerateDungeon();
+        }
+
+        private void GenerateDungeon()
+        {
+            for (int i = 0; i < totalRooms; i++)
+            {
+                var parent = new GameObject();
+                parent.transform.SetParent(transform);
+
+                var room = ScriptableObject.CreateInstance<Room>();
+                var tiles = room.GenerateTiles();
+                var walls = room.GenerateWalls();
+                
+
+            }
+        }
+    }
+}
--- Assets/Scripts/Dungeon/Room.cs
+++ Assets/Scripts/Dungeon/Room.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour {
+namespace Dungeon
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public class Room : ScriptableObject
+    {
+        public int width, height;
+        [MinMaxRange(4, 10)]
+        public RangedFloat widthRange;
+        [MinMaxRange(4, 10)]
+        public RangedFloat heightRange;
+
+        public Transform[,] tiles;
+
+        private void OnEnable()
+        {
+            width = (int)Random.Range(widthRange.minValue, widthRange.maxValue);
+            height = (int)Random.Range(heightRange.minValue, heightRange.maxValue);
+        }
+
+        public GameObject[,] GenerateTiles()
+        {
+            GameObject[,] tiles = new GameObject[width, height];
+
+            for (int w = 0; w < width; w++)
+            {
+                for (int h = 0; h < height; h++)
+                {
+                    var tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    tile.transform.position = new Vector2(w, h);
+                    tiles[w, h] = tile;
+                }
+            }
+
+            return tiles;
+        }
+
+        public GameObject[] GenerateWalls()
+        {
+            GameObject[] walls = new GameObject[width * 2 + height * 2 + 4];
+
+            return walls;
+        }
+    }
 }

f92b1aac23e2dfc5d3e65262b6d47db0486ffd3a
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-06-17 17:11:11 +0100

    Adapted the terrain editor script so I can generate dungeons in editor now, much quicker workflow. It also destroys any existing dungeon objects so it has a clean scene to work with.

---
+++ Assets/Editor/DungeonGeneratorEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+using Dungeon;
+
+[CustomEditor(typeof(DungeonGenerator))]
+public class DungeonGeneratorEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DungeonGenerator dungeonGen = (DungeonGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (dungeonGen.autoUpdate)
+            {
+                RemoveChildren(dungeonGen);
+                dungeonGen.Generate();
+            }
+        }
+
+        if (GUILayout.Button("Randomise Seed"))
+        {
+            var rand = new System.Random();
+            dungeonGen.seed = rand.Next();
+            RemoveChildren(dungeonGen);
+            dungeonGen.Generate();
+        }
+        if (GUILayout.Button("Generate"))
+        {
+            RemoveChildren(dungeonGen);
+            dungeonGen.Generate();
+        }
+    }
+
+    private void RemoveChildren(DungeonGenerator generator)
+    {
+        List<GameObject> destroyMe = new List<GameObject>();
+
+        var parent = generator.dungeonParent;
+        for (int i = parent.childCount; i > 0; i--)
+        {
+            var child = parent.GetChild(0); // Safer to go back to front?
+            DestroyImmediate(child.gameObject);
+        }
+    }
+}

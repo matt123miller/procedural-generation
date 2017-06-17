73a1eccfbfd10a33fe5c2ed8d7f5e18da5378d3a
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-06-17 17:10:18 +0100

    Wrote a more strongly typed Layer wrapper so there's a single point of truth now. I don't like having to remember strings in line and risk typos.

---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Layers{
+
+    public static int Wall = LayerMask.NameToLayer("Wall");
+    public static int Floor = LayerMask.NameToLayer("Floor");
+    public static int Eenmy = LayerMask.NameToLayer("Enemy");
+    public static int Friendly = LayerMask.NameToLayer("Friendly");
+    public static int Obstacle = LayerMask.NameToLayer("Obstacle");
+
+    //public static int IntFromLayerMask
+}

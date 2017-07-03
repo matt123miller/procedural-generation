cefc1e42ca80510248ff123ead642ab68f6ef7ab
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-07-03 22:51:40 +0100

    Made a corridor, secrelty it's just a longer room.

---
+++ Assets/Scripts/Dungeon/Corridor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Dungeon;
+
+public class Corridor : Room
+{
+    public override void SetupRoom(int _width, int _height, Material _material)
+    {
+        bool horizontal = Random.Range(0, 2) % 2 == 0;
+        int w, h;
+
+        if (horizontal)
+        {
+            w = _width * 2;
+            h = 4;
+        }
+        else
+        {
+            w = 4;
+            h = _height * 2;
+        }
+        print("LOL THIS IS A CORRIDOR NOT A ROOM, WHAT A TROLL!");
+        base.SetupRoom(w, h, _material);
+    }
+
+}

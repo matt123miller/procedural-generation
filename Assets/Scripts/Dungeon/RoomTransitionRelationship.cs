e46ca3f54c7b875b600f35740800146aae8c5a8b
Matt Miller <matt123miller@hotmail.co.uk>
2019-04-20 14:42:27 +0100

    Wrote some handy extras on my DSL RoomTransitionRelationship.

--- Assets/Scripts/Dungeon/RoomTransitionRelationship.cs
+++ Assets/Scripts/Dungeon/RoomTransitionRelationship.cs
@@ -15,11 +15,19 @@ namespace Dungeon
             this.via = via;
         }
 
+        public RoomTransitionRelationship Flip()
+        {
+            return new RoomTransitionRelationship(this.to, this.from, OppositeDirection());
+        }
+
+        public Vector3 OppositeDirection()
+        {
+            return Vector3.Reflect(via, via);
+        }
+
         public override string ToString()
         {
-
             var str = $"Travelling from room {from.roomID} to room {to.roomID} in the direction of {via}";
-
 
             return str;
         }
